0c350d89a769b0ea4ee95d22cde3a536fae8972a
bretuobay <bretuobay@gmail.com>
2016-09-19 22:31:26 +0200

    fixed routing /session issues

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 
+error_reporting(0);
+
 session_start();
+
 require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.php';
 
 define( 'PATH_BASE', dirname(__FILE__) );
@@ -16,43 +19,28 @@ if (isset($_SESSION['curr_user']) && isset($_SESSION['logged_in'])) {
 
     if (isset($_GET['section'])) {
 
-        //header('location:dashboard.php');
-        //FIXME cannot do double redirect , if session is set let the code handle it
-
     } else {
         header('location:front_app.php');
         exit;
     }
 }
 
-//var_dump($_SESSION);
-
 $router = !isset($_GET['section'])? 'calories' : $_GET['section'];
 
 $action = isset($_GET['do'])? $_GET['do']: 'home';
 
-
-//var_dump($router);
-//var_dump($action);
 
 $controller = ucfirst($router).'Controller.php';
 $model = ucfirst($router).'.php';
 
 $controllerName = explode('.',$controller)[0];
 
-//var_dump($controller);
-
-
-// Check for the file
 if(file_exists(PATH_BASE.DS.CONTROLLERS.DS.$controller)) {
-    // models any of the models can be loaded later for use, no need loading them here
-    //include  PATH_BASE.DS.MODELS.DS.$model;
     include  PATH_BASE.DS.CONTROLLERS.DS.$controller;
 } else {
     return false;
 }
 
-// Initialize and execute
 $contrl = new $controllerName($action);
 
 $contrl->$action();
